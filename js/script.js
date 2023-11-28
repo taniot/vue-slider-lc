78902d9df38f5bdff93d9492505d6e9558090a4e
taniot <gaetano.frascolla@gmail.com>
2023-11-28 10:49:55 +0100

    feat: thumbnails and active

--- js/script.js
+++ js/script.js
@@ -9,34 +9,6 @@ Bonus:
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
 
-const slides = [
-  {
-    image: 'img/01.webp',
-    title: "Marvel's Spiderman Miles Morale",
-    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-  },
-  {
-    image: 'img/02.webp',
-    title: 'Ratchet & Clank: Rift Apart',
-    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-  },
-  {
-    image: 'img/03.webp',
-    title: 'Fortnite',
-    text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
-  },
-  {
-    image: 'img/04.webp',
-    title: 'Stray',
-    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-  },
-  {
-    image: 'img/05.webp',
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
-
 // console.log(slides);
 //destrutturiamo createApp
 const { createApp } = Vue;
@@ -75,6 +47,7 @@ const vueConfig = {
     return {
       title,
       slides,
+      prova: 'pippo',
     };
   },
 };
@@ -83,3 +56,12 @@ const vueConfig = {
 const myApp = createApp(vueConfig);
 //montiamo app nel dom
 myApp.mount('#app');
+
+// function sum2Number(num1, num2) {}
+
+// const num1 = prompt('Inserisci il tuo numero');
+// const num2 = prompt('Inserisci il tuo numero');
+
+// sum2Number(num1, num2);
+
+// // sum2Number(prompt('Inserisci il tuo numero'), prompt('Inserisci il tuo numero'))
